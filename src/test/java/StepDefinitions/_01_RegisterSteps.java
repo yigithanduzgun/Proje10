ff1713ae9d8049e4967c864c8535be793d2d23f4
SevgiDereli <derelisevgi93@gmail.com>
2026-06-19 21:21:00 +0300

    Stepsler eklendi.

---
+++ src/test/java/StepDefinitions/_01_RegisterSteps.java
@@ -0,0 +1,33 @@
+package StepDefinitions;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_RegisterSteps {
+    @Given("User clicks Register")
+    public void userClicksRegister() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("User fills registration form")
+    public void userFillsRegistrationForm() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("User clicks Register button")
+    public void userClicksRegisterButton() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("Account should be create")
+    public void accountShouldBeCreate() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
